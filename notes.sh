dd18c69f0817e9e7cf622b6f699925d2a9e748cb
wang-chonghuan <wangchonghuan@gmail.com>
2023-03-10 23:21:58 +0000

    clean last commit

--- notes.sh
+++ notes.sh
@@ -1,3 +1,4 @@
 npx create-react-app super-forum-client --template typescript
+cd super-forum-client
 npm i redux react-redux @types/redux @types/react-redux react-router-dom @types/react-router-dom
 npm start
